bbe069a2e514460abddb316a0454307a583f5c62
BSOstore <webe2gether@gmail.com>
2026-05-31 13:32:16 -0700

    Tune temporary insert measurement scale

--- vision.py
+++ vision.py
@@ -49,7 +49,7 @@ def process_hoof_image(input_path, output_path):
 
     cv2.imwrite(output_path, output_img)
 
-    pixels_per_mm = 0.95
+    pixels_per_mm = 0.96
 
     measured_width = round(w / pixels_per_mm)
     measured_length = round(h / pixels_per_mm)
